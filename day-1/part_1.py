90056c25a8ed5bed574d068963f22d9805cd34cb
Mikael Lind <elemel@elemel.se>
2018-12-01 22:22:46 +0100

    Solve day 1

---
+++ day-1/part_1.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def main():
+	print(sum((int(line) for line in sys.stdin)))
+
+
+if __name__ == '__main__':
+	main()
---
+++ day-1/part_2.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def main():
+	sequence = [int(line) for line in sys.stdin]
+	current = 0
+	seen = set([current])
+	duplicate = None
+
+	while not duplicate:
+		for change in sequence:
+			current += change
+
+			if current in seen:
+				duplicate = current
+				break
+
+			seen.add(current)
+
+	print(duplicate)
+
+
+if __name__ == '__main__':
+	main()
