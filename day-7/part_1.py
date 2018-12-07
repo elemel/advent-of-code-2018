abc3009275a97c5e6d4f2db5e79b97b7e79d6c73
Mikael Lind <elemel@elemel.se>
2018-12-07 09:34:12 +0100

    Spaces not tabs

--- day-7/part_1.py
+++ day-7/part_1.py
@@ -4,41 +4,41 @@ from sys import stdin
 
 
 def parse_requirement(line):
-	words = line.split()
-	return words[1], words[-3]
+    words = line.split()
+    return words[1], words[-3]
 
 
 def main():
-	requirements = [parse_requirement(line) for line in stdin]
+    requirements = [parse_requirement(line) for line in stdin]
 
-	children = defaultdict(set)
-	parents = defaultdict(set)
+    children = defaultdict(set)
+    parents = defaultdict(set)
 
-	for parent, child in requirements:
-		children[parent].add(child)
-		parents[child].add(parent)
+    for parent, child in requirements:
+        children[parent].add(child)
+        parents[child].add(parent)
 
-	available = []
+    available = []
 
-	for parent in children:
-		if not parents[parent]:
-			heappush(available, parent)
+    for parent in children:
+        if not parents[parent]:
+            heappush(available, parent)
 
-	completed = []
+    completed = []
 
-	while available:
-		parent = heappop(available)
+    while available:
+        parent = heappop(available)
 
-		for child in children[parent]:
-			parents[child].remove(parent)
+        for child in children[parent]:
+            parents[child].remove(parent)
 
-			if not parents[child]:
-				heappush(available, child)
+            if not parents[child]:
+                heappush(available, child)
 
-		completed.append(parent)
+        completed.append(parent)
 
-	print(''.join(completed))
+    print(''.join(completed))
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- day-7/part_2.py
+++ day-7/part_2.py
@@ -4,51 +4,51 @@ from sys import stdin
 
 
 def parse_requirement(line):
-	words = line.split()
-	return words[1], words[-3]
+    words = line.split()
+    return words[1], words[-3]
 
 
 def get_duration(step):
-	return 60 + ord(step) - ord('A') + 1
+    return 60 + ord(step) - ord('A') + 1
 
 
 def main():
-	requirements = [parse_requirement(line) for line in stdin]
+    requirements = [parse_requirement(line) for line in stdin]
 
-	children = defaultdict(set)
-	parents = defaultdict(set)
+    children = defaultdict(set)
+    parents = defaultdict(set)
 
-	for parent, child in requirements:
-		children[parent].add(child)
-		parents[child].add(parent)
+    for parent, child in requirements:
+        children[parent].add(child)
+        parents[child].add(parent)
 
-	available = []
+    available = []
 
-	for parent in children:
-		if not parents[parent]:
-			heappush(available, parent)
+    for parent in children:
+        if not parents[parent]:
+            heappush(available, parent)
 
-	second = 0
-	in_progress = []
-	completed = []
+    second = 0
+    in_progress = []
+    completed = []
 
-	while available or in_progress:
-		while available and len(in_progress) < 5:
-			parent = heappop(available)
-			heappush(in_progress, (second + get_duration(parent), parent))
+    while available or in_progress:
+        while available and len(in_progress) < 5:
+            parent = heappop(available)
+            heappush(in_progress, (second + get_duration(parent), parent))
 
-		second, parent = heappop(in_progress)
+        second, parent = heappop(in_progress)
 
-		for child in children[parent]:
-			parents[child].remove(parent)
+        for child in children[parent]:
+            parents[child].remove(parent)
 
-			if not parents[child]:
-				heappush(available, child)
+            if not parents[child]:
+                heappush(available, child)
 
-		completed.append(parent)
+        completed.append(parent)
 
-	print(second)
+    print(second)
 
 
 if __name__ == '__main__':
-	main()
+    main()
