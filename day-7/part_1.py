5f9af07cbb474282f10eb014a5b4058b6c9bc404
Mikael Lind <elemel@elemel.se>
2018-12-07 09:30:44 +0100

    Solve day 7

---
+++ day-7/part_1.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from heapq import heappop, heappush
+from sys import stdin
+
+
+def parse_requirement(line):
+	words = line.split()
+	return words[1], words[-3]
+
+
+def main():
+	requirements = [parse_requirement(line) for line in stdin]
+
+	children = defaultdict(set)
+	parents = defaultdict(set)
+
+	for parent, child in requirements:
+		children[parent].add(child)
+		parents[child].add(parent)
+
+	available = []
+
+	for parent in children:
+		if not parents[parent]:
+			heappush(available, parent)
+
+	completed = []
+
+	while available:
+		parent = heappop(available)
+
+		for child in children[parent]:
+			parents[child].remove(parent)
+
+			if not parents[child]:
+				heappush(available, child)
+
+		completed.append(parent)
+
+	print(''.join(completed))
+
+
+if __name__ == '__main__':
+	main()
---
+++ day-7/part_2.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+from heapq import heappop, heappush
+from sys import stdin
+
+
+def parse_requirement(line):
+	words = line.split()
+	return words[1], words[-3]
+
+
+def get_duration(step):
+	return 60 + ord(step) - ord('A') + 1
+
+
+def main():
+	requirements = [parse_requirement(line) for line in stdin]
+
+	children = defaultdict(set)
+	parents = defaultdict(set)
+
+	for parent, child in requirements:
+		children[parent].add(child)
+		parents[child].add(parent)
+
+	available = []
+
+	for parent in children:
+		if not parents[parent]:
+			heappush(available, parent)
+
+	second = 0
+	in_progress = []
+	completed = []
+
+	while available or in_progress:
+		while available and len(in_progress) < 5:
+			parent = heappop(available)
+			heappush(in_progress, (second + get_duration(parent), parent))
+
+		second, parent = heappop(in_progress)
+
+		for child in children[parent]:
+			parents[child].remove(parent)
+
+			if not parents[child]:
+				heappush(available, child)
+
+		completed.append(parent)
+
+	print(second)
+
+
+if __name__ == '__main__':
+	main()
