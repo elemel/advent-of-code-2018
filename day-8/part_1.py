e3e0ad179e6263d2e7f6b27a465ed3acafaf2aa1
Mikael Lind <elemel@elemel.se>
2018-12-08 13:59:14 +0100

    Spaces not tabs

--- day-8/part_1.py
+++ day-8/part_1.py
@@ -2,18 +2,18 @@ from sys import stdin
 
 
 def solve(node):
-	child_count = node.pop()
-	metadata_count = node.pop()
+    child_count = node.pop()
+    metadata_count = node.pop()
 
-	return (sum(solve(node) for _ in range(child_count)) +
-		sum(node.pop() for _ in range(metadata_count)))
+    return (sum(solve(node) for _ in range(child_count)) +
+        sum(node.pop() for _ in range(metadata_count)))
 
 
 def main():
-	node = [int(s) for s in stdin.read().split()]
-	node.reverse()
-	print(solve(node))
+    node = [int(s) for s in stdin.read().split()]
+    node.reverse()
+    print(solve(node))
 
 
 if __name__ == '__main__':
-	main()
+    main()
