ed643bc919198e71d5f0f4053915c9be55c9f2a7
Mikael Lind <elemel@elemel.se>
2018-12-09 15:07:06 +0100

    Deque the halls

--- day-9/part_1.py
+++ day-9/part_1.py
@@ -1,11 +1,5 @@
+from collections import deque
 from sys import stdin
-
-
-class Link:
-    def __init__(self, value=None, previous=None, next=None):
-        self.value = value
-        self.previous = previous
-        self.next = next
 
 
 def main():
@@ -13,27 +7,18 @@ def main():
     player_count = int(words[0])
     last_marble = int(words[-2])
 
-    scores = [0 for _ in range(player_count)]
+    scores = player_count * [0]
     player = 0
-    circle = Link(value=0)
-    circle.previous = circle
-    circle.next = circle
+    circle = deque([0])
 
     for marble in range(1, last_marble + 1):
         if marble % 23 == 0:
             scores[player] += marble
-
-            for i in range(6):
-                circle = circle.previous
-
-            scores[player] += circle.previous.value
-            circle.previous.previous.next = circle
-            circle.previous = circle.previous.previous
+            circle.rotate(7)
+            scores[player] += circle.popleft()
         else:
-            circle = circle.next
-            circle = Link(value=marble, previous=circle, next=circle.next)
-            circle.previous.next = circle
-            circle.next.previous = circle
+            circle.rotate(-2)
+            circle.appendleft(marble)
 
         player = (player + 1) % player_count
 
