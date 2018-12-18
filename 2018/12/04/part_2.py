475742d8ef945560a26f8f1815969fe7d0e282f0
Mikael Lind <elemel@elemel.se>
2018-12-18 22:32:21 +0100

    Simplify some more

--- 2018/12/04/part_2.py
+++ 2018/12/04/part_2.py
@@ -2,34 +2,26 @@ from collections import Counter
 from sys import stdin
 
 
-def parse_guard_id(line):
-    return int(line[26:-13])
-
-
-def parse_minute(line):
-    return int(line[15:17])
-
-
 def main():
     lines = sorted(line.strip() for line in stdin)
 
     guard_id = None
     falls_asleep_minute = None
 
-    guard_minute_counter = Counter()
+    counter = Counter()
 
     for line in lines:
         if line.endswith(' begins shift'):
-            guard_id = parse_guard_id(line)
+            guard_id = int(line[26:-13])
         elif line.endswith(' falls asleep'):
-            falls_asleep_minute = parse_minute(line)
+            falls_asleep_minute = int(line[15:17])
         elif line.endswith(' wakes up'):
-            wakes_up_minute = parse_minute(line)
+            wakes_up_minute = int(line[15:17])
 
             for minute in range(falls_asleep_minute, wakes_up_minute):
-                guard_minute_counter[guard_id, minute] += 1
+                counter[guard_id, minute] += 1
 
-    ((guard_id, minute), _), = guard_minute_counter.most_common(1)
+    ((guard_id, minute), _), = counter.most_common(1)
     print(guard_id * minute)
 
 
