a2b1b1041b8cddaf2ee0c55c9cefd0b02ea4b269
Mikael Lind <elemel@elemel.se>
2018-12-18 21:12:03 +0100

    Use counters

--- 2018/12/18/part_2.py
+++ 2018/12/18/part_2.py
@@ -1,36 +1,37 @@
-from sys import maxsize, stdin
-
-
-def print_map(map_):
-    for row in map_:
-        print(row)
+from collections import Counter
+from sys import stdin
 
 
 def change_acre(map_, x, y):
+    acre = map_[y][x]
+
     min_x = max(0, x - 1)
     min_y = max(0, y - 1)
     max_x = min(x + 1, len(map_[0]) - 1)
     max_y = min(y + 1, len(map_) - 1)
 
-    adjacent = ''.join(sorted(
+    adjacent = Counter(
         map_[y2][x2]
         for y2 in range(min_y, max_y + 1)
         for x2 in range(min_x, max_x + 1)
-        if (x2, y2) != (x, y)))
-
-    acre = map_[y][x]
+        if (x2, y2) != (x, y))
 
     if acre == '.':
-        if '|||' in adjacent:
+        if adjacent['|'] >= 3:
             acre = '|'
     elif acre == '|':
-        if '###' in adjacent:
+        if adjacent['#'] >= 3:
             acre = '#'
     elif acre == '#':
         if '#' not in adjacent or '|' not in adjacent:
             acre = '.'
 
     return acre
+
+
+def print_map(map_):
+    for row in map_:
+        print(row)
 
 
 def main():
@@ -58,9 +59,8 @@ def main():
 
     # print_map(map_)
 
-    wooded_acres = sum(acre == '|' for row in map_ for acre in row)
-    lumberyards = sum(acre == '#' for row in map_ for acre in row)
-    print(wooded_acres * lumberyards)
+    resources = Counter(acre for row in map_ for acre in row)
+    print(resources['|'] * resources['#'])
 
 
 if __name__ == '__main__':
