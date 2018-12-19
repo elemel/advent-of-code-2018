bca5bfb2595d63e3d6359231cc71782ec20d8c51
Mikael Lind <elemel@elemel.se>
2018-12-19 21:14:49 +0100

    Clean up day 6 solutions

--- 2018/12/06/part_1.py
+++ 2018/12/06/part_1.py
@@ -1,4 +1,10 @@
-from sys import maxint, stdin
+from collections import Counter
+from sys import maxsize, stdin
+
+
+def parse_location(line):
+    x, y = [int(s) for s in line.split(',')]
+    return x, y
 
 
 def manhattan_distance(x1, y1, x2, y2):
@@ -6,39 +12,43 @@ def manhattan_distance(x1, y1, x2, y2):
 
 
 def main():
-    locations = [tuple(int(s) for s in line.split(',')) for line in stdin]
+    locations = [parse_location(line.strip()) for line in stdin]
 
     min_x = min(x for x, _ in locations)
     min_y = min(y for _, y in locations)
     max_x = max(x for x, _ in locations)
     max_y = max(y for _, y in locations)
 
-    sizes = len(locations) * [0]
-    infinite = len(locations) * [False]
+    area_sizes = Counter()
+    infinite_areas = set()
 
-    for x1 in range(min_x, max_x + 1):
-        for y1 in range(min_y, max_y + 1):
-            closest_indices = []
-            min_distance = maxint
+    for y1 in range(min_y, max_y + 1):
+        for x1 in range(min_x, max_x + 1):
+            closest_locations = set()
+            min_distance = maxsize
 
-            for i, (x2, y2) in enumerate(locations):
+            for x2, y2 in locations:
                 distance = manhattan_distance(x1, y1, x2, y2)
 
                 if distance < min_distance:
-                    del closest_indices[:]
+                    closest_locations.clear()
                     min_distance = distance
 
                 if distance == min_distance:
-                    closest_indices.append(i)
+                    closest_locations.add((x2, y2))
 
-            if len(closest_indices) == 1:
-                sizes[closest_indices[0]] += 1
+            if len(closest_locations) == 1:
+                [closest_location] = closest_locations
+                area_sizes[closest_location] += 1
 
-            if x1 == min_x or x1 == max_x or y1 == min_y or y1 == max_y:
-                for i in closest_indices:
-                    infinite[i] = True
+            if x1 in [min_x, max_x] or y1 in [min_y, max_y]:
+                infinite_areas |= closest_locations
 
-    print(max(size for i, size in enumerate(sizes) if not infinite[i]))
+    for location in infinite_areas:
+        area_sizes.pop(location, None)
+
+    [(_, largest_area_size)] = area_sizes.most_common(1)
+    print(largest_area_size)
 
 
 if __name__ == '__main__':
--- 2018/12/06/part_2.py
+++ 2018/12/06/part_2.py
@@ -1,4 +1,9 @@
-from sys import maxint, stdin
+from sys import stdin
+
+
+def parse_location(line):
+    x, y = [int(s) for s in line.split(',')]
+    return x, y
 
 
 def manhattan_distance(x1, y1, x2, y2):
@@ -6,7 +11,7 @@ def manhattan_distance(x1, y1, x2, y2):
 
 
 def main():
-    locations = [tuple(int(s) for s in line.split(',')) for line in stdin]
+    locations = [parse_location(line.strip()) for line in stdin]
 
     min_x = min(x for x, _ in locations)
     min_y = min(y for _, y in locations)
@@ -15,8 +20,8 @@ def main():
 
     print(sum(
         sum(manhattan_distance(x1, y1, x2, y2) for x2, y2 in locations) < 10000
-        for x1 in range(min_x, max_x + 1)
-        for y1 in range(min_y, max_y + 1)))
+        for y1 in range(min_y, max_y + 1)
+        for x1 in range(min_x, max_x + 1)))
 
 
 if __name__ == '__main__':
