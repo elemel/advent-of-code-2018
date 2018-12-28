7bc82a4df0729d113b2102722f456dba3286014d
Mikael Lind <elemel@elemel.se>
2018-12-28 16:31:35 +0100

    Solve day 10 of 2015

---
+++ 2015/10/part_2.py
@@ -0,0 +1,17 @@
+from itertools import chain, groupby
+from sys import stdin
+
+
+def main():
+    sequence = [int(c) for c in stdin.read().strip()]
+
+    for _ in range(50):
+        sequence = list(chain.from_iterable(
+            (len(list(g)), k)
+            for k, g in groupby(sequence)))
+
+    print(len(sequence))
+
+
+if __name__ == '__main__':
+    main()
