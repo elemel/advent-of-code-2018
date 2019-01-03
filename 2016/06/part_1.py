23a383407b3632f1b46f33ca2609564a62afeb83
Mikael Lind <elemel@elemel.se>
2019-01-03 20:28:03 +0100

    Solve day 6 of 2016

---
+++ 2016/06/part_1.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from sys import stdin
+
+
+def main():
+    messages = [line.strip() for line in stdin]
+    counters = [Counter() for _ in range(8)]
+
+    for message in messages:
+        for i, character in enumerate(message):
+            counters[i][character] += 1
+
+    print(''.join(counter.most_common(1)[0][0] for counter in counters))
+
+
+if __name__ == '__main__':
+    main()
