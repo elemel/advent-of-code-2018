87d63f81c3bdcf234d9fc2524f340fc14835a539
Mikael Lind <elemel@elemel.se>
2019-01-06 17:22:16 +0100

    Solve day 16 of 2016

---
+++ 2016/16/part_2.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+
+def main():
+    initial_state = stdin.read().strip()
+    a = list(initial_state)
+
+    while len(a) < 35651584:
+        b = list('1' if c == '0' else '0' for c in reversed(a))
+        a.append('0')
+        a.extend(b)
+
+    a = a[:35651584]
+
+    checksum = a
+
+    while len(checksum) % 2 == 0:
+        checksum = [
+            '1' if a == b else '0'
+            for a, b in zip(checksum[::2], checksum[1::2])
+        ]
+
+    print(''.join(checksum))
+
+
+if __name__ == '__main__':
+    main()
