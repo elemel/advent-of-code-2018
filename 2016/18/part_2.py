37d32388a26454acc4bd3a73928c49179be8a325
Mikael Lind <elemel@elemel.se>
2019-01-06 22:29:31 +0100

    Solve day 18 of 2016

---
+++ 2016/18/part_2.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+
+rules = {
+    '^^.': '^',
+    '.^^': '^',
+    '^..': '^',
+    '..^': '^',
+}
+
+
+def main():
+    row = stdin.read().strip()
+    rows = [row]
+
+    while len(rows) < 400000:
+        padded_row = '.' + row + '.'
+
+        row = ''.join(
+            rules.get(padded_row[i:(i + 3)], '.') for i in range(len(row)))
+
+        rows.append(row)
+
+    print(sum(sum(tile == '.' for tile in row) for row in rows))
+
+
+if __name__ == '__main__':
+    main()
