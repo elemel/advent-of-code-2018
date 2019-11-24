98ad8b958cc35f1f438f350f06ff6164917c1ddd
Mikael Lind <elemel@elemel.se>
2019-11-24 20:34:16 +0100

    Add more unit tests

--- lua/yulea/test/runTests.lua
+++ lua/yulea/test/runTests.lua
@@ -1,13 +1,30 @@
 local ansicolors = require("yulea.test.ansicolors")
 
+local function sortedPairs(t)
+  local keys = {}
+
+  for k, v in pairs(t) do
+    table.insert(keys, k)
+  end
+
+  table.sort(keys)
+
+  return coroutine.wrap(function()
+    for _, k in ipairs(keys) do
+      coroutine.yield(k, t[k])
+    end
+  end)
+end
+
 local testSuites = {
   iteratorTest = require("yulea.test.iteratorTest"),
+  mathTest = require("yulea.test.mathTest"),
   stringTest = require("yulea.test.stringTest"),
   tableTest = require("yulea.test.tableTest"),
 }
 
-for testSuiteName, testCases in pairs(testSuites) do
-  for testCaseName, testCase in pairs(testCases) do
+for testSuiteName, testCases in sortedPairs(testSuites) do
+  for testCaseName, testCase in sortedPairs(testCases) do
     local testName = testSuiteName .. "." .. testCaseName
 
     if pcall(testCase) then
